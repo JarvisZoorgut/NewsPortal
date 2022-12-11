9f176889d685a09952d08e5c4df5287e25b84016
ivan <jarvis.zoorgut@gmail.com>
2022-12-11 20:23:27 +0500

    D11

--- NewsPortal/news_app/admin.py
+++ NewsPortal/news_app/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('postTime', 'title', 'rating', 'author') #не смог добавить категорию
+    list_filter = ('postTime', 'rating', 'author')
+    search_fields = ('postTime', 'rating', 'author')
 
-admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('authorUser', 'ratingAuthor')
+    list_filter = ('authorUser', 'ratingAuthor')
+    search_fields = ('authorUser', 'ratingAuthor')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('commentTime', 'comment', 'rating', 'commentUser', 'commentPost') #как сделать по title поста?
+    list_filter = ('commentTime', 'comment', 'rating', 'commentUser', 'commentPost')
+    search_fields = ('commentTime', 'comment', 'rating', 'commentUser', 'commentPost')
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
